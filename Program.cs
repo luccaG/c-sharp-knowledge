9dff6e586a0648009bb34bbe461b13fa61cb9c8d
Lucas Giannico <lucas.giannico.a@gmail.com>
2019-06-15 09:36:14 -0300

    primer commit de core escuela

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela{
+        public string nombre;
+         public string direccion;
+
+        public int añoFundacion;
+         public void Timbrar(){
+            //todo
+            Console.Beep(2000 ,3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+             var escuelita = new Escuela();
+             escuelita.nombre="Chinga Tu Madre";
+             escuelita.direccion="A la verga ";     
+             escuelita.añoFundacion=2003;
+             Console.WriteLine(" a la huewva ");
+             escuelita.Timbrar();
+             
+                }
+    }
+}
